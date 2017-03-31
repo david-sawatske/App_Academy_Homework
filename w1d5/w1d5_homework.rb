4503f9f5bdc027700f77c80446a513e0fc2a85c2
David Sawatske <sawatskeda10@gmail.com>
2017-03-31 07:30:25 -0700

    Complete EX 3

--- w1d5/w1d5_homework.rb
+++ w1d5/w1d5_homework.rb
@@ -35,3 +35,34 @@ class Queue
     @queue.dup
   end
 end
+
+# EX 3
+class Map
+  def initialize
+    @map = []
+  end
+
+  def set(key, value)
+    # Do maps have ordered indices?
+    @map.delete_if { |pair| pair[0] == key }
+    @map[key] = value
+  end
+
+  def get(key)
+    @map.select { |pair| pair[0] == key }
+    nil
+  end
+
+  def delete(key)
+    @map.delete_if { |pair| pair[0] == key }
+  end
+
+  def show
+    deep_dup(@map)
+  end
+
+  private
+  def deep_dup(arr)
+    arr.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
+  end
+end
