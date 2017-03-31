0eb5adaaa60fcdf7871194f722f1769faced1352
David Sawatske <sawatskeda10@gmail.com>
2017-03-31 06:39:53 -0700

    Complete EX 2

--- w1d5/w1d5_homework.rb
+++ w1d5/w1d5_homework.rb
@@ -16,3 +16,22 @@ class Stack
     @stack.dup
   end
 end
+
+# EX 2
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue << el
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def show
+    @queue.dup
+  end
+end
