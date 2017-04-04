4fc281d26a820617451fbad6bedec6c91ae7f8c4
David Sawatske <sawatskeda10@gmail.com>
2017-04-04 07:26:07 -0700

    Complete w2d2 Mancala homework

---
+++ w2d2/mancala/skeleton/lib/board.rb
@@ -0,0 +1,69 @@
+class Board
+  attr_accessor :cups
+
+  def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) { Array.new }
+  end
+
+  def place_stones
+    @cups.each.with_index do |cup, index|
+      unless idx == 6 || idx == 13
+        4.times do
+          cup << :stone
+        end
+      end
+    end
+  end
+
+  def valid_move?(start_pos)
+    raise "Cup is not on the board" if start_pos < 0 || start_pos > 12
+    raise "That cup is empty" if @cups[start_pos].empty?
+  end
+
+  def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+
+    stones.count.times do |idx|
+      start_pos += idx
+      cup_idx = 0 if cup_idx > 13
+
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop
+      end
+    end
+  end
+
+  def next_turn(ending_cup_idx)
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+    @cups[0..6].all? { |cup| cup.empty? } ||
+    @cups[7..12].all? { |cup| cup.empty? }
+  end
+
+  def winner
+    @cups[6].count > @cups[13].count ? @name1 : @name2
+  end
+end
